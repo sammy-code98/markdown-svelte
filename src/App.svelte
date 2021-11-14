9ac69c690b400f9e5e0b1ebe149cf11ad20b594f
sammy-code98 <sammysmart98@yahoo.com>
2021-11-14 21:51:04 +0100

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,98 @@
 <script>
-	export let name;
+import marked from 'marked';
+
+	let source = `
+	# H1 heading
+
+	## H2 heading
+
+	### H3 heading
+
+	---------
+
+	**bold text**
+
+	*Italiced text*
+
+	---------
+
+	1. first item
+	2. second item
+
+	- first item
+	- second item
+	- third item
+
+	[Svelte](https://svelte.dev/)
+
+
+	`
+
+	let markdown = marked(source)
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<header class="header">
+		<h1 class="header-title">Svelte markdown editor</h1>
+	</header>
+	
+	<div class="markdown-editor">
+		<div class="left-panel">
+			<textarea bind:value={source} class="source"></textarea>
+		</div>
+	
+		<div class="right-panel">
+			<div class="output">{@html markdown}</div>
+		</div>
+	</div>
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	.container{
+        background: #ff3e00d6;
+        padding:10px 30px;
+    }
+    .header {
+        height: 10vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    }
+    .header-title {
+        margin: 0;
+        color:#fff;
+    }
+    .markdown-editor {
+        width: 100%;
+        display: flex;
+        align-items:flex-start;
+        justify-content: space-evenly;
+    }
+    .left-panel, .right-panel {
+        width: 50%;
+        border: solid 1px black;
+        height: 85vh;
+        background: #ffffff;
+    }
+    .right-panel {
+        overflow: auto;
+    }
+    .source {
+        border: none;
+        width: 100%;
+        height: 100%;
+        background: #001628;
+        color: #83ba52;
+    }
+    .source:focus {
+        outline: none;
+    }
+    .output {
+        width: 100%;
+        padding: 0 2em;
+    }
 
 	@media (min-width: 640px) {
 		main {
