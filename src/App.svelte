57da88194146cabbaaf082db2e49c6d088f35afe
sammy-code98 <sammysmart98@yahoo.com>
2021-11-14 21:56:29 +0100

    fix: fix import issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-import marked from 'marked';
+  import { marked } from "marked";
 
-	let source = `
+  let source = `
 	# H1 heading
 
 	## H2 heading
@@ -26,77 +26,76 @@ import marked from 'marked';
 	[Svelte](https://svelte.dev/)
 
 
-	`
+	`;
 
-	let markdown = marked(source)
+  let markdown = marked(source);
 </script>
 
 <main class="container">
-	<header class="header">
-		<h1 class="header-title">Svelte markdown editor</h1>
-	</header>
-	
-	<div class="markdown-editor">
-		<div class="left-panel">
-			<textarea bind:value={source} class="source"></textarea>
-		</div>
-	
-		<div class="right-panel">
-			<div class="output">{@html markdown}</div>
-		</div>
-	</div>
-	
+  <header class="header">
+    <h1 class="header-title">Svelte markdown editor</h1>
+  </header>
+
+  <div class="markdown-editor">
+    <div class="left-panel">
+      <textarea bind:value={source} class="source" />
+    </div>
+
+    <div class="right-panel">
+      <div class="output">{@html markdown}</div>
+    </div>
+  </div>
 </main>
 
 <style>
-	.container{
-        background: #ff3e00d6;
-        padding:10px 30px;
-    }
-    .header {
-        height: 10vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  .container {
+    background: #ff3e00d6;
+    padding: 10px 30px;
+  }
+  .header {
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .header-title {
+    margin: 0;
+    color: #fff;
+  }
+  .markdown-editor {
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-evenly;
+  }
+  .left-panel,
+  .right-panel {
+    width: 50%;
+    border: solid 1px black;
+    height: 85vh;
+    background: #ffffff;
+  }
+  .right-panel {
+    overflow: auto;
+  }
+  .source {
+    border: none;
+    width: 100%;
+    height: 100%;
+    background: #001628;
+    color: #83ba52;
+  }
+  .source:focus {
+    outline: none;
+  }
+  .output {
+    width: 100%;
+    padding: 0 2em;
+  }
 
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
     }
-    .header-title {
-        margin: 0;
-        color:#fff;
-    }
-    .markdown-editor {
-        width: 100%;
-        display: flex;
-        align-items:flex-start;
-        justify-content: space-evenly;
-    }
-    .left-panel, .right-panel {
-        width: 50%;
-        border: solid 1px black;
-        height: 85vh;
-        background: #ffffff;
-    }
-    .right-panel {
-        overflow: auto;
-    }
-    .source {
-        border: none;
-        width: 100%;
-        height: 100%;
-        background: #001628;
-        color: #83ba52;
-    }
-    .source:focus {
-        outline: none;
-    }
-    .output {
-        width: 100%;
-        padding: 0 2em;
-    }
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  }
 </style>
