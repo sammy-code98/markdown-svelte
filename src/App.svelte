e57f7f1077575835f51824d80c0dac43a93e5c4d
sammy-code98 <sammysmart98@yahoo.com>
2021-11-18 22:09:18 +0100

    feat:remove unused code - bind source to textarea - add @html

--- src/App.svelte
+++ src/App.svelte
@@ -3,57 +3,13 @@
   let source = `# New Document`;
   let markdown = marked(source) 
 
-  // let source = `
-	// # H1 heading
-
-	// ## H2 heading
-
-	// ### H3 heading
-
-	// ---------
-
-	// **bold text**
-
-	// *Italiced text*
-
-	// ---------
-
-	// 1. first item
-	// 2. second item
-
-	// - first item
-	// - second item
-	// - third item
-
-	// [Svelte](https://svelte.dev/)
-
-
-	// `;
-
-  // let markdown = marked(source);
 </script>
-
- <!-- <main class="container">
-  <header class="header">
-    <h1 class="header-title">Svelte markdown editor</h1>
-  </header>
-
-  <div class="markdown-editor">
-    <div class="left-panel">
-      <textarea bind:value={source} class="source" />
-    </div>
-
-    <div class="right-panel">
-      <div class="output">{@html markdown}</div>
-    </div>
-  </div> 
-</main>  -->
 <header class="header">
   <h1 class="header-title">Markdown editor</h1>
 </header>
 <div class="markdown-editor">
   <div class="markdown-editor_leftpanel">
-    <textarea  class="markdown-editor_source"></textarea>
+    <textarea bind:value={source} class="markdown-editor_source"></textarea>
   </div>
   <div class="markdown-editor_rightpanel">
     <div class="markdown-editor_output">{@html markdown}</div>
@@ -61,51 +17,6 @@
 </div>
 
 <style>
-  /* .container {
-    background: #ff3e00d6;
-    padding: 10px 30px;
-  }
-  .header {
-    height: 10vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .header-title {
-    margin: 0;
-    color: #fff;
-  }
-  .markdown-editor {
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-evenly;
-  }
-  .left-panel,
-  .right-panel {
-    width: 50%;
-    border: solid 1px black;
-    height: 85vh;
-    background: #ffffff;
-  }
-  .right-panel {
-    overflow: auto;
-  }
-  .source {
-    border: none;
-    width: 100%;
-    height: 100%;
-    background: #001628;
-    color: #83ba52;
-  }
-  .source:focus {
-    outline: none;
-  }
-  .output {
-    width: 100%;
-    padding: 0 2em;
-  } */
-
   .header {
         height: 10vh;
         display: flex;
